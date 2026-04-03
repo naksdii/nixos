dc79959bae36b0f1c2ea92582b2561574d56a1a5
naksdii <vini112341@gmail.com>
2026-04-03 19:18:42 -0300

    w 	modified:   home/dotfiles.nix

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,38 +1,31 @@
-{ ... }:
+{ config, pkgs, ... }:
+
 {
   xdg.configFile = {
     #########
-    #folders#
+    # FOLDERS (Cópia Recursiva)
     #########
+    # Ao usar 'source' em uma pasta e não definir nada extra, o Nix costuma linkar a pasta.
+    # Se quiser que os arquivos dentro sejam editáveis, o Nix ainda vai tentar protegê-los.
     "fastfetch".source = ./dotfiles/fastfetch;
     "matugen".source = ./dotfiles/matugen;
 
     ########
-    #waybar#
+    # ARQUIVOS (Passando o conteúdo)
     ########
-    "waybar/config.jsonc".source = ./dotfiles/waybar/config.jsonc;
-    "waybar/style.css".source = ./dotfiles/waybar/style.css;
-    "waybar/modules.json".source = ./dotfiles/waybar/modules.json;
+    "waybar/config.jsonc".text = builtins.readFile ./dotfiles/waybar/config.jsonc;
+    "waybar/style.css".text    = builtins.readFile ./dotfiles/waybar/style.css;
+    "waybar/modules.json".text = builtins.readFile ./dotfiles/waybar/modules.json;
 
-    ########
-    #swaync#
-    ########
-    "swaync/config.json".source = ./dotfiles/swaync/config.json;
-    "swaync/style.css".source = ./dotfiles/swaync/style.css;
+    "swaync/config.json".text  = builtins.readFile ./dotfiles/swaync/config.json;
+    "swaync/style.css".text   = builtins.readFile ./dotfiles/swaync/style.css;
 
-    ######
-    #rofi#
-    ######
-    "rofi/config.rasi".source = ./dotfiles/rofi/config.rasi;
-    "rofi/themes/round.rasi".source = ./dotfiles/rofi/themes/round.rasi;
+    "rofi/config.rasi".text    = builtins.readFile ./dotfiles/rofi/config.rasi;
+    "rofi/themes/round.rasi".text = builtins.readFile ./dotfiles/rofi/themes/round.rasi;
 
-
-    ########################
-    #single archive configs#
-    ########################
-    "kitty/kitty.conf".source = ./dotfiles/kitty/kitty.conf;
-    "helix/config.toml".source = ./dotfiles/helix/config.toml;
-    "cava/config".source = ./dotfiles/cava/config;
-    "hyfetch.json".source = ./dotfiles/hyfetch.json;
+    "kitty/kitty.conf".text    = builtins.readFile ./dotfiles/kitty/kitty.conf;
+    "helix/config.toml".text   = builtins.readFile ./dotfiles/helix/config.toml;
+    "cava/config".text        = builtins.readFile ./dotfiles/cava/config;
+    "hyfetch.json".text       = builtins.readFile ./dotfiles/hyfetch.json;
   };
 }
